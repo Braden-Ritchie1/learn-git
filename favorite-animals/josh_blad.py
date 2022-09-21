7fbc7f6626b29ef524474a0086f824e5580d9bb7
mountaindew33 <joshblad99@gmail.com>
2022-09-21 14:58:45 -0600

    add josh_blad file

---
+++ favorite-animals/josh_blad.py
@@ -0,0 +1,9 @@
+# Josh Blad
+# A loop
+
+iFavTeam = input("Enter your favorite NFL team: ")
+
+if iFavTeam == "Seahawks":
+    print("That is correct!")
+else :
+    print("You chose the wrong team. The correct answer is 'Seahawks'")
